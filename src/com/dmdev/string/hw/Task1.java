293fd058b81eca300ec967eac9edef9775e138e1
Kova <a.g.bezrukov@gmail.com>
2023-02-20 21:34:25 +0600

    Initial commit

---
+++ src/com/dmdev/string/hw/Task1.java
@@ -0,0 +1,42 @@
+package com.dmdev.string.hw;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+/**
+ * Дана строка.
+ * Удалить из нее все повторяющиеся символы без учета регистра, если они идут друг за другом.
+ * Также удалить пробелы.
+ * Результат привести к верхнему регистру.
+ * <p>
+ * Например:
+ * "abc Cpddd Dio OsfWw" -> "ABCPDIOSFW"
+ */
+public class Task1 {
+    private static final String SPACE = " ";
+    private static final String EMPTY = "";
+
+    public static void main(String[] args) {
+        String value = "abc Cpddd Dio OsfWw";
+
+        System.out.println(removeDuplicateCharacter(value));
+    }
+
+    private static String removeDuplicateCharacter(String value) {
+        String withoutSpaceValue = value.replace(SPACE, EMPTY);
+        String withUpperCase = withoutSpaceValue.toUpperCase();
+
+        StringBuilder s_buffer = new StringBuilder(withUpperCase.subSequence(0, withUpperCase.length()));
+
+        int c = s_buffer.length() - 1;
+
+        for (int i = 0; i < c; i++) {
+            if (s_buffer.charAt(i) == s_buffer.charAt(i + 1)) {
+                s_buffer.deleteCharAt(i + 1);
+                i -= 1;
+                c -= 1;
+            }
+        }
+        return String.valueOf(s_buffer);
+    }
+}
---
+++ src/com/dmdev/string/hw/Task2.java
@@ -0,0 +1,63 @@
+package com.dmdev.string.hw;
+
+/**
+ * Дана строка с текстом, в котором есть цифры от 0 до 9.
+ * <p>
+ * Написать 2 метода:
+ * <p>
+ * - возвращающий массив цифр из переданной строки
+ * - возвращающий сумму цифр из переданного целочисленного массива
+ * <p>
+ * Посчитать сумму всех чисел из строки
+ * <p>
+ * Например:
+ * “Привет 8, как 1 2 твои дела? Может4, сделать 3 дело?” -> 18 (8+1+2+4+3)
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        String value = "Привет 8, как 1 2 твои дела? Может4, сделать 3 дело?";
+
+        int[] resultArray = arrayDigitsFromString(value);
+        int sumArrayDigits = sumArrayDigits(resultArray);
+
+        System.out.println(sumArrayDigits);
+    }
+
+    private static int sumArrayDigits(int[] value) {
+        int sumValue = 0;
+
+        for (int i : value) {
+            sumValue += i;
+        }
+        return sumValue;
+    }
+
+    private static int[] arrayDigitsFromString(String value) {
+
+        int count = 0;
+        char[] valueArrayString = value.toCharArray();
+        int[] valueArrayIntFromString = new int[arraySizeIntFromString(value)];
+
+        for (int i = 0; i < valueArrayString.length; i++) {
+            if (valueArrayString[i] == '0' || valueArrayString[i] == '1' || valueArrayString[i] == '2' ||
+                    valueArrayString[i] == '3' || valueArrayString[i] == '4' || valueArrayString[i] == '5' ||
+                    valueArrayString[i] == '6' || valueArrayString[i] == '7' || valueArrayString[i] == '8' ||
+                    valueArrayString[i] == '9') {
+
+                valueArrayIntFromString[count] = Integer.parseInt(String.valueOf(valueArrayString[i]));
+                count++;
+            }
+        }
+        return valueArrayIntFromString;
+    }
+
+    private static int arraySizeIntFromString(String value) {
+        String result = value.replace("0", "")
+                .replace("1", "").replace("2", "")
+                .replace("3", "").replace("4", "")
+                .replace("5", "").replace("6", "")
+                .replace("7", "").replace("8", "")
+                .replace("9", "");
+        return value.length() - result.length();
+    }
+}
